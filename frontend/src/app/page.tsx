d0a9950a3b17fd7212ed1883b55960e48661a32d
BijeshNeupane <neubij64@gmail.com>
2026-07-28 21:01:16 +0545

    project initialization

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      A Next level POS system
+    </div>
+  );
+};
+
+export default page;
