29f0bf12856927b371c10695652d19c0975da537
BijeshNeupane <neubij64@gmail.com>
2026-07-31 20:41:22 +0545

    connected backend and frontend

--- backend/test_api/urls.py
+++ backend/test_api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test/', include('test_api.urls')),
+    path('check/', views.demo, name='test'),
 ]
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
-const page = () => {
+const page = async () => {
+  const res = await fetch("http://localhost:8000/test/check/");
+  const data = await res.json();
+  console.log("response is", data);
+
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       A Next level POS system
